c2937a72519cfeaba516e9efed69e36e929358e9
Anubhav Yadav <anubhav1691@gmail.com>
2018-05-14 17:59:33 +0530

    Add tests for `get_lists` in `Trello`.

--- tests/test_trello.py
+++ tests/test_trello.py
@@ -1,4 +1,28 @@
 import pytest
 import responses
+import re
 
-# tests for trello.py [TODO]
+from create_trello_cards.trello import Trello
+
+
+@pytest.fixture()
+def trello_obj():
+    return Trello(trello_key="fookey", trello_token="footoken")
+
+
+@pytest.fixture()
+def trello_url():
+    return "https://api.trello.com/*"
+
+
+@responses.activate
+def test_get_lists(trello_obj, trello_url):
+    expected = [{'id': '5af97c9dbbe346788853b3ba',
+                 'name': 'test',
+                 'closed': False,
+                 'idBoard': '5af97addbaeeb13712b04ed1',
+                 'pos': 81919,
+                 'subscribed': False}]
+    responses.add(responses.GET, re.compile(trello_url), json=expected, status=200)
+    response = trello_obj.get_lists(board_id='fooid')
+    assert response == expected
