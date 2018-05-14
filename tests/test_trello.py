6b12d2d9dceff4335aaf22dbfc0845a23fcd240f
Anubhav Yadav <anubhav1691@gmail.com>
2018-05-14 18:18:06 +0530

    Add tests for `get_cards` in `Trello`.
    Also added a `conftest` in the tests directory to specify default fixtures, to load the test directory. The test directory now contains mock json for the responses to be expected from trello api.

--- tests/test_trello.py
+++ tests/test_trello.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 import pytest
 import responses
 import re
@@ -15,6 +18,11 @@ def trello_url():
     return "https://api.trello.com/*"
 
 
+@pytest.fixture()
+def json_directory(setup_tests_directory):
+    return os.path.join(setup_tests_directory, "fixtures")
+
+
 @responses.activate
 def test_get_lists(trello_obj, trello_url):
     expected = [{'id': '5af97c9dbbe346788853b3ba',
@@ -25,4 +33,13 @@ def test_get_lists(trello_obj, trello_url):
                  'subscribed': False}]
     responses.add(responses.GET, re.compile(trello_url), json=expected, status=200)
     response = trello_obj.get_lists(board_id='fooid')
+    assert response == expected
+
+
+@responses.activate
+def test_get_cards(trello_obj, trello_url, json_directory):
+    with open(os.path.join(json_directory, 'cards.json')) as f:
+        expected = json.load(f)
+    responses.add(responses.GET, url=re.compile(trello_url), json=expected, status=200)
+    response = trello_obj.get_cards(list_id='fooid')
     assert response == expected
